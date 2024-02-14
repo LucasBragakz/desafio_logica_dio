f89745c41b41ae7b92e2c49fa9411ce41b8cafdb
LucasBragakz <lucaskz2@hotmail.com>
2024-02-14 20:27:53 -0300

    Desafio oferecido pela dio (criar variaveis para armazenar Nome do heroi e XP para compara com patentes e concatenar as informaçoes no final .

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hello Word");
+let nomeHeroi ="kakaroto"
+let xpHeroi =9000
+let patente
+
+if(xpHeroi<=1000){
+    patente = "Ferro"
+}
+else if(xpHeroi>1000 && xpHeroi<=2000){
+    patente = "Bronze"
+}
+else if(xpHeroi>2000 && xpHeroi<=5000){
+    patente = "Prata"
+}
+else if(xpHeroi>5000 && xpHeroi<=7000){
+    patente = "Ouro"
+}
+else if(xpHeroi>8000 && xpHeroi<=8000){
+    patente = "Platina"
+}
+else if(xpHeroi>8000 && xpHeroi<=9000){
+    patente = "Ascendente"
+}
+else if(xpHeroi>9000 && xpHeroi<=10000){
+    patente = "Imortal"
+}
+else{
+    patente = "Radiante"
+}
+
+console.log("O Herói de nome "+ nomeHeroi + " está no nível de "+ patente + " com "+ xpHeroi+" de XP")
